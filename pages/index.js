5e063bc67cbfded50c44b3c49ba269ac182f4398
Spas <12554483+Spasmochi@users.noreply.github.com>
2020-08-13 20:37:42 +1000

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="bg-blue-100">Hello</main>
+    </div>
+  );
+}
