49eedde405d332a86700ee4d6d31778c8d439f07
Spas <12554483+Spasmochi@users.noreply.github.com>
2020-08-13 20:51:21 +1000

    Added nice welcome component

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="bg-blue-100">Hello</main>
+      <main>
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8 h-screen flex items-center">
+          <div className="max-w-3xl mx-auto">
+            <div className="flex items-center bg-indigo-600 justify-center py-12 md:px-24 px-20 rounded-md">
+              <h1 className="text-center text-3xl leading-10 font-bold  text-gray-200 sm:text-4xl sm:leading-10">
+                Happy hacking!
+              </h1>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
